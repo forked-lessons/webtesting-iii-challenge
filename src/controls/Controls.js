2a17cdfab9df7471dabc5d9ce381edf883cbee3e
Daniel <danielstarling4@gmail.com>
2019-04-10 15:42:41 -0700

    tests added

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -5,10 +5,20 @@ const Controls = props => {
 
   return (
     <div className="controls panel">
-      <button disabled={!closed} onClick={toggleLocked} className="toggle-btn">
+      <button
+        disabled={!closed}
+        onClick={toggleLocked}
+        className="toggle-btn"
+        data-testid="lockToggle"
+      >
         {locked ? 'Unlock Gate' : 'Lock Gate'}
       </button>
-      <button disabled={locked} onClick={toggleClosed} className="toggle-btn">
+      <button
+        disabled={locked}
+        onClick={toggleClosed}
+        className="toggle-btn"
+        data-testid="gateToggle"
+      >
         {closed ? 'Open Gate' : 'Close Gate'}
       </button>
     </div>
